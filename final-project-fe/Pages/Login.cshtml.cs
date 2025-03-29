1196646de6abac37bbc6877c2784e65a326eddf1
BinhNguyen2311 <nguyenphuocbinh2311@gmail.com>
2025-03-29 19:18:23 +0700

    Implement Front End

---
+++ final-project-fe/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace final_project_fe.Pages.Shared
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ final-project-fe/Pages/UserPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace final_project_fe.Pages
+{
+    public class UserPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
