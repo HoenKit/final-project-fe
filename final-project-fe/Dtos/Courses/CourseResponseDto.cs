f14f040e3aa0b27d8c46cb6123dd13f10a4c7363
HoenKit <hoangkaito03@gmail.com>
2025-06-12 20:22:36 +0700

    Filter and Pagination Course Page

---
+++ final-project-fe/Dtos/Courses/CourseResponseDto.cs
@@ -0,0 +1,19 @@
+﻿namespace final_project_fe.Dtos.Courses
+{
+	public class CourseResponseDto
+	{
+		public int CourseId { get; set; }
+		public int MentorId { get; set; }
+		public int CategoryId { get; set; }
+		public string CourseName { get; set; }
+		public string CourseContent { get; set; }
+		public decimal Cost { get; set; }
+		public string SkillLearn { get; set; }
+		public int StudentCount { get; set; }
+		public string? CoursesImage { get; set; }
+		public double? CourseLength { get; set; }
+		public DateTime? UpdateAt { get; set; }
+		public int CountModule { get; set; }
+		public int CountLesson { get; set; }
+	}
+}
---
+++ final-project-fe/Dtos/Courses/GetCourseDto.cs
@@ -0,0 +1,18 @@
+﻿using final_project_fe.Dtos.Mentors;
+
+namespace final_project_fe.Dtos.Courses
+{
+	public class GetCourseDto
+	{
+		public int CourseId { get; set; }
+		public string CourseName { get; set; }
+		public string CourseContent { get; set; }
+		public decimal Cost { get; set; }
+		public string SkillLearn { get; set; }
+		public int StudentCount { get; set; }
+		public string? CoursesImage { get; set; }
+		public double? CourseLength { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public MentorDto? Mentor { get; set; }
+	}
+}
