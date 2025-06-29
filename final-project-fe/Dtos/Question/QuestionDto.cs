a647b58ce2c8c4f42ff2192c4717cd3011527a56
BinhNguyen2311 <nguyenphuocbinh2311@gmail.com>
2025-06-29 16:33:18 +0700

    CRUD Question
    CRUD Question

---
+++ final-project-fe/Dtos/Question/QuestionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace final_project_fe.Dtos.Question
+{
+    public class QuestionDto
+    {
+        public int LessonId { get; set; }
+        public string Question_text { get; set; }
+        public string QuestionType { get; set; }
+    }
+}
