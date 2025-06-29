354092851c34eaf2ead8fc9e15b9c1bc9a678b09
Tatsu64 <155626068+Tatsu64@users.noreply.github.com>
2025-06-29 13:34:53 +0700

    Add Transaction History This Month in Dashboard

---
+++ final-project-fe/Dtos/Transaction/GetTransactionDto.cs
@@ -0,0 +1,17 @@
+﻿using final_project_fe.Dtos.Users;
+
+namespace final_project_fe.Dtos.Transaction
+{
+    public class GetTransactionDto
+    {
+        public int TransactionId { get; set; }
+        public Guid UserId { get; set; }
+        public decimal? Points { get; set; }
+        public string? PaymentMethod { get; set; }
+        public string? Status { get; set; }
+        public decimal? Amount { get; set; }
+        public string? OrderCode { get; set; }
+        public DateTime CreateAt { get; set; }
+        public User? User { get; set; }
+    }
+}
