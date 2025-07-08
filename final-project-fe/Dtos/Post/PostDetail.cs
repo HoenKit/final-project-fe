2c497bebf07326d175b3dc1ce97598f26a10766f
Tatsu64 <155626068+Tatsu64@users.noreply.github.com>
2025-07-08 18:06:10 +0700

    New Page Post Detail

---
+++ final-project-fe/Dtos/Post/PostDetail.cs
@@ -0,0 +1,22 @@
+﻿using final_project_fe.Dtos.Comment;
+
+namespace final_project_fe.Dtos.Post
+{
+    public class PostDetail
+    {
+        public int PostId { get; set; }
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Avatar { get; set; }
+        public int? ParentPostId { get; set; }
+        public string CategoryName { get; set; }
+        public bool? IsDeleted { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
+        public List<PostFileDto>? PostFiles { get; set; }
+        public List<CommentPostDetailDto>? Comments { get; set; }
+    }
+}
